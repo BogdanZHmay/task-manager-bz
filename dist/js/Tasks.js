277f04e246505e4c1e03b15a5b6186d70738b06a
Bogdan_ZHmay <morellozhmay@gmail.com>
2017-09-23 11:26:07 +0300

    change color theme

--- dist/js/Tasks.js
+++ dist/js/Tasks.js
@@ -13,7 +13,9 @@
                 .then(this.render)
                 .then(this.addEvent)
                 .then(Tasks.hidePreloader)
-                .catch(this.logout);
+                .then(this.addNewTask)
+                .then(this.changeColorTheme)
+                .catch(this.errorHandler);
         }
 
         checkToken(self){
@@ -23,11 +25,11 @@
                 let index = document.cookie.indexOf('_id');
                 let indexEnd = document.cookie.indexOf(';', index);
                 let token = indexEnd === -1 ?
-                                document.cookie.slice(index) :
-                                    document.cookie.slice(index, indexEnd);
+                    document.cookie.slice(index) :
+                    document.cookie.slice(index, indexEnd);
 
                 if(index !== -1) resolve(self);
-                else reject('Text not found');
+                else reject('logout');
             })
         }
 
@@ -87,6 +89,24 @@
             console.log('Rendering tasks...', self);
 
             return new Promise(function (resolve, reject) {
+
+                let activeTaskContainer = $('#active-task .container');
+                let doneTaskContainer = $('#all-task .container');
+
+                let getActiveTaskHtml = self.undoneTasks ?
+                    Tasks.renderTaskWrap(self.undoneTasks) : '';
+                let getDoneTaskHtml = self.doneTasks ?
+                    Tasks.renderTaskWrap(self.doneTasks) : '';
+
+                activeTaskContainer.append(getActiveTaskHtml);
+                doneTaskContainer.append(getDoneTaskHtml);
+
+                if(!self.undoneTasks){
+                    Tasks.emptyTasks(activeTaskContainer, 'У вас нет активных задач')
+                }
+                if (!self.doneTasks){
+                    Tasks.emptyTasks(doneTaskContainer, 'У вас нет завершенных задач')
+                }
                 resolve(self);
             })
         }
@@ -97,6 +117,47 @@
             $('.task-header').on('click', Tasks.taskAccordion)
 
         }
+
+        errorHandler(type){
+            if(type === 'logout'){
+                this.logout();
+            }else{
+                console.error('Error');
+            }
+        }
+
+        changeColorTheme(){
+            let li = $('.change-color li');
+            let blocks = $('[data-change-bg]');
+            let color = localStorage.getItem('color') || '#00a1f1';
+
+            function setColor(color) {
+                blocks.each(function (i, e) {
+                    $(e).css("background-color", color);
+                });
+            }
+
+            li.each(function(i, e) {
+                let color = $(e).attr('data-color');
+                $(e).css("background-color", color);
+            });
+
+            li.on('click', function() {
+                let color = $(this).attr('data-color');
+                setColor(color);
+                localStorage.setItem('color', color);
+
+            });
+
+            setColor(color);
+        }
+
+        // addNewTask(task){
+        //     this.modal = $('.edit-task');
+        //     this.TaskBtn = $('.add-task');
+        //     this.overlay = $('.overlay');
+        //
+        // }
 
         static hidePreloader(){
 
@@ -127,6 +188,116 @@
             }
 
         }
+
+        static renderTaskWrap(obj){
+            let marcup = '';
+
+            for( let day in obj){
+                marcup += `<div>
+                                <div class="task-day" data-change="background-color" data-change-bg>
+                                    <span class="day">${obj[day][0].day}</span>
+                                    <span class="month">${obj[day][0].month}</span>
+                                </div>
+
+                                <div class="all-task-wrap">
+                                    ${Tasks.renderTasksByDay(obj[day])}
+                                </div>
+                            </div>`
+            }
+
+            return marcup;
+        }
+
+        static renderTasksByDay(array){
+            let tasksMarcup = '';
+            let date =Date.now();
+
+            array.forEach(task =>{
+
+                let taskWarning = date > task.ms ? 'warning' : '';
+                let taskStatus = task.status === 'done' ? 'success' : '';
+
+                tasksMarcup += `<div class="task ${taskStatus || taskWarning}">
+						<div class="to-do-time" data-change-bg>${task.time}</div>
+						<div class="task-header flex-container">
+							<span class="icon icon-arrow"></span>
+							<span class="icon short-task-text">${task.taskText}</span>
+							<span class="icon icon-cancel"></span>
+						</div>
+						<div class="task-content-wrap">
+							<div class="time-row flex-container">
+								<div class="task-icon">
+									<span class="icon icon-time"></span>	
+								</div>
+								<div class="time">${task.time}</div>
+								<div class="task-icon">
+									<span class="icon icon-bell"></span>
+								</div>
+							</div>
+							<div class="task-text-row flex-container">
+								<div class="task-icon">
+									<span class="icon icon-list"></span>
+								</div>
+								<div class="text">${task.taskText}</div>
+							</div>
+							<div class="task-check-row check-done flex-container">
+								<div class="task-icon">
+									<span class="icon icon-check"></span>
+								</div>
+								<div class="check">
+									<input type="checkbox" name="status" id="status">
+									<label for="status">Я выполнил задачу</label>
+								</div>
+							</div>
+							<div class="task-check-row check-current flex-container">
+								<div class="task-icon">
+									<span class="icon icon-check"></span>
+								</div>
+								<div class="check">
+									<input type="checkbox" name="status" id="status">
+									<label for="status">Здача еще не выполнена</label>
+								</div>
+							</div>
+							<div class="task-edit-row flex-container">
+								<span class="icon icon-edit"></span>
+							</div>
+							
+						</div>
+						<div class="task-message deadline">
+							<div class="message-row flex-container">
+								<div class="task-icon">
+									<span class="icon icon-warning"></span>
+								</div>
+								<div class="text">
+									<p>Вы не выполнили задачу</p>
+								</div>
+							</div>
+						</div>
+						<div class="task-message done">
+							<div class="message-row flex-container">
+								<div class="task-icon">
+									<span class="icon icon-star"></span>
+								</div>
+								<div class="text">
+									<p>Поздравляем!</p>
+									<p>Вы справились с задачей</p>
+								</div>
+							</div>
+						</div>
+					</div>`
+
+            });
+
+            return tasksMarcup;
+        }
+
+        static emptyTasks(container, text){
+
+            let alert = `<div class="alert empty-task"> ${text} </div>`;
+
+            container.append(alert);
+        }
+
     }
 
     let tasks = new Tasks();
--- server/asset/js/Tasks.js
+++ server/asset/js/Tasks.js
@@ -15,7 +15,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
             value: function init() {
                 var self = this;
 
-                this.checkToken(self).then(this.getTasks).then(this.sortTask).then(this.render).then(this.addEvent).then(Tasks.hidePreloader).catch(this.logout);
+                this.checkToken(self).then(this.getTasks).then(this.sortTask).then(this.render).then(this.addEvent).then(Tasks.hidePreloader).then(this.addNewTask).then(this.changeColorTheme).catch(this.errorHandler);
             }
         }, {
             key: 'checkToken',
@@ -27,7 +27,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                     var indexEnd = document.cookie.indexOf(';', index);
                     var token = indexEnd === -1 ? document.cookie.slice(index) : document.cookie.slice(index, indexEnd);
 
-                    if (index !== -1) resolve(self);else reject('Text not found');
+                    if (index !== -1) resolve(self);else reject('logout');
                 });
             }
         }, {
@@ -94,6 +94,22 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                 console.log('Rendering tasks...', self);
 
                 return new Promise(function (resolve, reject) {
+
+                    var activeTaskContainer = $('#active-task .container');
+                    var doneTaskContainer = $('#all-task .container');
+
+                    var getActiveTaskHtml = self.undoneTasks ? Tasks.renderTaskWrap(self.undoneTasks) : '';
+                    var getDoneTaskHtml = self.doneTasks ? Tasks.renderTaskWrap(self.doneTasks) : '';
+
+                    activeTaskContainer.append(getActiveTaskHtml);
+                    doneTaskContainer.append(getDoneTaskHtml);
+
+                    if (!self.undoneTasks) {
+                        Tasks.emptyTasks(activeTaskContainer, 'У вас нет активных задач');
+                    }
+                    if (!self.doneTasks) {
+                        Tasks.emptyTasks(doneTaskContainer, 'У вас нет завершенных задач');
+                    }
                     resolve(self);
                 });
             }
@@ -104,6 +120,49 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
                 $('.task-header').on('click', Tasks.taskAccordion);
             }
+        }, {
+            key: 'errorHandler',
+            value: function errorHandler(type) {
+                if (type === 'logout') {
+                    this.logout();
+                } else {
+                    console.error('Error');
+                }
+            }
+        }, {
+            key: 'changeColorTheme',
+            value: function changeColorTheme() {
+                var li = $('.change-color li');
+                var blocks = $('[data-change-bg]');
+                var color = localStorage.getItem('color') || '#00a1f1';
+
+                function setColor(color) {
+                    blocks.each(function (i, e) {
+                        $(e).css("background-color", color);
+                    });
+                }
+
+                li.each(function (i, e) {
+                    var color = $(e).attr('data-color');
+                    $(e).css("background-color", color);
+                });
+
+                li.on('click', function () {
+                    var color = $(this).attr('data-color');
+                    setColor(color);
+                    localStorage.setItem('color', color);
+                });
+
+                setColor(color);
+            }
+
+            // addNewTask(task){
+            //     this.modal = $('.edit-task');
+            //     this.TaskBtn = $('.add-task');
+            //     this.overlay = $('.overlay');
+            //
+            // }
+
         }], [{
             key: 'hidePreloader',
             value: function hidePreloader() {
@@ -141,6 +200,41 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                         return $(parent).addClass('open');
                     });
                 }
+            }
+        }, {
+            key: 'renderTaskWrap',
+            value: function renderTaskWrap(obj) {
+                var marcup = '';
+
+                for (var day in obj) {
+                    marcup += '<div>\n                                <div class="task-day" data-change="background-color" data-change-bg>\n                                    <span class="day">' + obj[day][0].day + '</span>\n                                    <span class="month">' + obj[day][0].month + '</span>\n                                </div>\n\n                                <div class="all-task-wrap">\n                                    ' + Tasks.renderTasksByDay(obj[day]) + '\n                                </div>\n                            </div>';
+                }
+
+                return marcup;
+            }
+        }, {
+            key: 'renderTasksByDay',
+            value: function renderTasksByDay(array) {
+                var tasksMarcup = '';
+                var date = Date.now();
+
+                array.forEach(function (task) {
+
+                    var taskWarning = date > task.ms ? 'warning' : '';
+                    var taskStatus = task.status === 'done' ? 'success' : '';
+
+                    tasksMarcup += '<div class="task ' + (taskStatus || taskWarning) + '">\n\t\t\t\t\t\t<div class="to-do-time" data-change-bg>' + task.time + '</div>\n\t\t\t\t\t\t<div class="task-header flex-container">\n\t\t\t\t\t\t\t<span class="icon icon-arrow"></span>\n\t\t\t\t\t\t\t<span class="icon short-task-text">' + task.taskText + '</span>\n\t\t\t\t\t\t\t<span class="icon icon-cancel"></span>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class="task-content-wrap">\n\t\t\t\t\t\t\t<div class="time-row flex-container">\n\t\t\t\t\t\t\t\t<div class="task-icon">\n\t\t\t\t\t\t\t\t\t<span class="icon icon-time"></span>\t\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t<div class="time">' + task.time + '</div>\n\t\t\t\t\t\t\t\t<div class="task-icon">\n\t\t\t\t\t\t\t\t\t<span class="icon icon-bell"></span>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t<div class="task-text-row flex-container">\n\t\t\t\t\t\t\t\t<div class="task-icon">\n\t\t\t\t\t\t\t\t\t<span class="icon icon-list"></span>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t<div class="text">' + task.taskText + '</div>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t<div class="task-check-row check-done flex-container">\n\t\t\t\t\t\t\t\t<div class="task-icon">\n\t\t\t\t\t\t\t\t\t<span class="icon icon-check"></span>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t<div class="check">\n\t\t\t\t\t\t\t\t\t<input type="checkbox" name="status" id="status">\n\t\t\t\t\t\t\t\t\t<label for="status">\u042F \u0432\u044B\u043F\u043E\u043B\u043D\u0438\u043B \u0437\u0430\u0434\u0430\u0447\u0443</label>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t<div class="task-check-row check-current flex-container">\n\t\t\t\t\t\t\t\t<div class="task-icon">\n\t\t\t\t\t\t\t\t\t<span class="icon icon-check"></span>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t<div class="check">\n\t\t\t\t\t\t\t\t\t<input type="checkbox" name="status" id="status">\n\t\t\t\t\t\t\t\t\t<label for="status">\u0417\u0434\u0430\u0447\u0430 \u0435\u0449\u0435 \u043D\u0435 \u0432\u044B\u043F\u043E\u043B\u043D\u0435\u043D\u0430</label>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t<div class="task-edit-row flex-container">\n\t\t\t\t\t\t\t\t<span class="icon icon-edit"></span>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class="task-message deadline">\n\t\t\t\t\t\t\t<div class="message-row flex-container">\n\t\t\t\t\t\t\t\t<div class="task-icon">\n\t\t\t\t\t\t\t\t\t<span class="icon icon-warning"></span>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t<div class="text">\n\t\t\t\t\t\t\t\t\t<p>\u0412\u044B \u043D\u0435 \u0432\u044B\u043F\u043E\u043B\u043D\u0438\u043B\u0438 \u0437\u0430\u0434\u0430\u0447\u0443</p>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class="task-message done">\n\t\t\t\t\t\t\t<div class="message-row flex-container">\n\t\t\t\t\t\t\t\t<div class="task-icon">\n\t\t\t\t\t\t\t\t\t<span class="icon icon-star"></span>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t<div class="text">\n\t\t\t\t\t\t\t\t\t<p>\u041F\u043E\u0437\u0434\u0440\u0430\u0432\u043B\u044F\u0435\u043C!</p>\n\t\t\t\t\t\t\t\t\t<p>\u0412\u044B \u0441\u043F\u0440\u0430\u0432\u0438\u043B\u0438\u0441\u044C \u0441 \u0437\u0430\u0434\u0430\u0447\u0435\u0439</p>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>';
+                });
+
+                return tasksMarcup;
+            }
+        }, {
+            key: 'emptyTasks',
+            value: function emptyTasks(container, text) {
+
+                var alert = '<div class="alert empty-task"> ' + text + ' </div>';
+
+                container.append(alert);
             }
         }]);
 
